dee32ac4bf8536a4ca9e96f17f7aaff5cf49217e
IKSEONGKU <127182222+IKSEONGKU@users.noreply.github.com>
2025-04-08 01:42:01 +0900

    feat: 메인 액티비티에 MainNavHost 추가

--- app/src/main/java/com/example/kuitandroidapiexample/MainActivity.kt
+++ app/src/main/java/com/example/kuitandroidapiexample/MainActivity.kt
@@ -5,12 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.kuitandroidapiexample.navigation.MainNavHost
 import com.example.kuitandroidapiexample.ui.theme.KuitAndroidApiExampleTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +17,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             KuitAndroidApiExampleTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    MainNavHost(padding = innerPadding)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    KuitAndroidApiExampleTheme {
-        Greeting("Android")
     }
 }
