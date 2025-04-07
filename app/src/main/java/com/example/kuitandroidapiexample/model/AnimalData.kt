8e119a5d93498fc605a793aae0da6d36b0b434c4
IKSEONGKU <127182222+IKSEONGKU@users.noreply.github.com>
2025-04-08 00:00:36 +0900

    feat: Animal Data - type 타입, String -> AnimalType 으로 변경

--- app/src/main/java/com/example/kuitandroidapiexample/model/AnimalData.kt
+++ app/src/main/java/com/example/kuitandroidapiexample/model/AnimalData.kt
@@ -3,6 +3,6 @@ package com.example.kuitandroidapiexample.model
 data class AnimalData(
     val imageUrl: String,
     val name: String,
-    val type: String,
+    val type: AnimalType,
     val address: String
 )
