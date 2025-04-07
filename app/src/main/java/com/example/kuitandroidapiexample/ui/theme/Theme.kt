7442c2ffba1bcc637e6444a211594fcdebedcda9
IKSEONGKU <127182222+IKSEONGKU@users.noreply.github.com>
2025-04-07 23:30:25 +0900

    feat: Set up Color System

--- app/src/main/java/com/example/kuitandroidapiexample/ui/theme/Theme.kt
+++ app/src/main/java/com/example/kuitandroidapiexample/ui/theme/Theme.kt
@@ -9,7 +9,16 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.ui.platform.LocalContext
+
+object FindUTheme {
+    val colors: FindUColors
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalFindUColorsProvider.current
+
+}
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
